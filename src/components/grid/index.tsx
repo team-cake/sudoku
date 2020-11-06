77aca5e8814aa7d85a9b4384d642af7136375bbc
team-cake <eka.cheuk@gmail.com>
2020-11-07 00:49:44 +0100

    Stylized the grid en added to the main index.tsx

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,18 +1,19 @@
 import React, { Children, FC } from 'react';
+import { Container, Row } from './styles';
 
 const Grid: FC = () => {
 	// Other stuff on the way
 
 	return (
-		<div>
+		<Container>
 			{Children.toArray(
 				[...Array(9)].map((_, rowIndex) => (
-					<div>
-						{Children.toArray([...Array(9)].map((_, colIndex) => <div></div>))}
-					</div>
+					<Row>
+						{Children.toArray([...Array(9)].map((_, colIndex) => <div>0</div>))}
+					</Row>
 				))
 			)}
-		</div>
+		</Container>
 	);
 };
 
