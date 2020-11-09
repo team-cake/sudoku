5582a0528eab710e5d82d425fcc1eea95f0f7ca4
team-cake <eka.cheuk@gmail.com>
2020-11-09 15:19:21 +0100

    Set up a function to check all the combinations of numbers for sudoku

--- src/utils/fill-grid/index.ts
+++ src/utils/fill-grid/index.ts
@@ -12,6 +12,27 @@ const gridExample: GRID = [
 	[0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
 
-function fillGrid(grid: GRID) {}
+/**
+ * a backtracking / recursive function to check all the possible combinations of numbers until a solution is found
+ * @param grid 9x9 Sudoku grud
+ *
+ */
+function fillGrid(grid: GRID) {
+	let row = 0;
+	let col = 0;
+
+	for (let i = 0; i < 81; i++) {
+		row = Math.floor(i / 9);
+		col = i % 9;
+
+		if (grid[row][col] === 0) {
+			// Do things
+			// recursive loop
+			break;
+		}
+	}
+
+	grid[row][col] = 0;
+}
 
 export default fillGrid;
