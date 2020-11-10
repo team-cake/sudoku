330f6660daa681623383fa78536a70518b88c8d7
team-cake <eka.cheuk@gmail.com>
2020-11-10 12:33:38 +0100

    Wrote function to check value within square

---
+++ src/utils/is-in/square/index.ts
@@ -0,0 +1,13 @@
+import { Interface } from 'readline';
+import { NUMBERS, SQUARE } from '../../../typings';
+
+interface IInput {
+	square: SQUARE;
+	value: NUMBERS;
+}
+
+function isInSquare({ square, value }: IInput): boolean {
+	return [...square[0], ...square[1], ...square[2]].includes(value);
+}
+
+export default isInSquare;
