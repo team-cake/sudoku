7b0d40e8a2f6503ca7f0a25bf4e9bcf8b4292ddd
team-cake <eka.cheuk@gmail.com>
2020-11-10 13:33:56 +0100

    Importing createFullGrid and console log to check the function

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,24 +1,12 @@
 import React, { FC, Children } from 'react';
 
-import { fillGrid } from '../../utils';
-import { GRID } from '../../typings';
+import { createFullGrid } from '../../utils';
 
 import Block from './block';
 import { Container, Row } from './styles';
 
 const Grid: FC = () => {
-	const grid: GRID = [
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0, 0, 0],
-	];
-	fillGrid(grid);
+	const grid = createFullGrid();
 	console.log(grid);
 
 	return (
