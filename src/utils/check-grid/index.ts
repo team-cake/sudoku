ac1e87aa1eeae814958a912b2abf4841f1925932
team-cake <eka.cheuk@gmail.com>
2020-11-10 12:49:09 +0100

    Wrote a function to check if grid is full

---
+++ src/utils/check-grid/index.ts
@@ -0,0 +1,13 @@
+import { GRID } from '../../typings';
+
+/**
+ * A function to check if the grid is full
+ * @param grid A 9x9 array consisting of values from 0-9
+ */
+function checkGrid(grid: GRID): boolean {
+	for (let i = 0; i < 9; i++)
+		for (let j = 0; j < 9; j++) if (grid[i][j] === 0) return false;
+	return true;
+}
+
+export default checkGrid;
