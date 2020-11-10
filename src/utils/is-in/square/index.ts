dbf907cb16181a59f6a75419c1b95ca30214f7fc
team-cake <eka.cheuk@gmail.com>
2020-11-10 12:51:23 +0100

    Function explanations add

--- src/utils/is-in/square/index.ts
+++ src/utils/is-in/square/index.ts
@@ -6,6 +6,10 @@ interface IInput {
 	value: NUMBERS;
 }
 
+/**
+ * A function that returns true if the value is already being used in the current grid square
+ * @param param0 Object with 3x3 square and value
+ */
 function isInSquare({ square, value }: IInput): boolean {
 	return [...square[0], ...square[1], ...square[2]].includes(value);
 }
