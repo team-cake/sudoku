597329c04b860e01540c9d77e5fb11b22907b9c0
team-cake <eka.cheuk@gmail.com>
2020-11-10 11:59:48 +0100

    Export identifySquare

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as FillGrid } from './fill-grid';
 export * from './is-in';
 export { default as Shuffle } from './shuffle';
+export { default as identifySquare } from './identify-square';
