cb87cfd25df775ba73e3fdd8644f29703d6e4c67
team-cake <eka.cheuk@gmail.com>
2020-11-11 09:26:35 +0100

    Set base for solveGrid function

---
+++ src/utils/solve-grid/index.ts
@@ -0,0 +1,18 @@
+import global from '../../global';
+import { GRID, NUMBERS } from '../../typings';
+import {
+	checkGrid,
+	identifySquare,
+	isInCol,
+	isInRow,
+	isInSquare,
+} from '../../utils';
+
+const numbers: NUMBERS[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+/**
+ * A backtracking / recursive function to check all possible combinations of numbers until  a solution is found
+ * @param grid A 9x9 array consisting of values from 0-9
+ */
+function solveGrid(grid: GRID) {}
+
+export default solveGrid;
