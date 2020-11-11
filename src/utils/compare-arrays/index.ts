17e7d88a9bd9a052d23c4592cf1f03af54df0168
team-cake <eka.cheuk@gmail.com>
2020-11-11 10:41:26 +0100

    Add 'return false' so popup 'you did it!'  doesn't appear everytime you fill in a correct number in a selected block, only when whole sudoku puzzle is completed

--- src/utils/compare-arrays/index.ts
+++ src/utils/compare-arrays/index.ts
@@ -6,9 +6,9 @@
 function compareArrays(arr1: any[], arr2: any[]): boolean {
 	if (!Array.isArray(arr1) && !Array.isArray(arr2)) return arr1 === arr2;
 
-	if (arr1.length !== arr2.length)
-		for (let i = 0, len = arr1.length; i < len; i++)
-			if (!compareArrays(arr1[i], arr2[i])) return false;
+	if (arr1.length !== arr2.length) return false;
+	for (let i = 0, len = arr1.length; i < len; i++)
+		if (!compareArrays(arr1[i], arr2[i])) return false;
 
 	return true;
 }
