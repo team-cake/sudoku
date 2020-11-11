b875cd2cf00aedc7381bccbed9842cb70296a323
team-cake <eka.cheuk@gmail.com>
2020-11-11 10:18:54 +0100

    Functionality to fill in possible numbers using redux with actions and types

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,5 +1,11 @@
 import { AnyAction } from 'redux';
-import { copyGrid, createFullGrid, removeNumbers } from '../utils';
+import { GRID } from '../typings';
+import {
+	compareArrays,
+	copyGrid,
+	createFullGrid,
+	removeNumbers,
+} from '../utils';
 
 import { IReducer } from './interfaces';
 import * as types from './types';
@@ -24,6 +30,21 @@ function reducer(state = initialState, action: AnyAction): IReducer {
 				workingGrid,
 			};
 		}
+
+		case types.FILL_BLOCK: {
+			if (state.workingGrid && state.solvedGrid) {
+				if (state.solvedGrid[action.coords[0]][action.coords[1]] !== action.value) {
+					alert('Incorrect option!');
+					return state;
+				}
+				state.workingGrid[action.coords[0]][action.coords[1]] = action.value;
+				if (compareArrays(state.workingGrid, state.solvedGrid))
+					alert('Yes, you did it!');
+				return { ...state, workingGrid: [...state.workingGrid] as GRID };
+			}
+			return state;
+		}
+
 		case types.SELECT_BLOCK:
 			return {
 				...state,
