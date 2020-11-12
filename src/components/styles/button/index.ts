c3d84045f494035047db722bd620192fb6fa3669
team-cake <eka.cheuk@gmail.com>
2020-11-12 12:55:12 +0100

    Adjusted typo's

--- src/components/styles/button/index.ts
+++ src/components/styles/button/index.ts
@@ -6,25 +6,26 @@ export const Button = styled.button`
 		background-color: ${theme.colors.black};
 		border: 2px solid ${theme.colors.black};
 		border-radius: 4px;
-        colors: ${theme.colors.white};
-        cursor: pointer;
-        display: flex;
-        font-size: 16px;
-        font-weight: bold;
-        height: 40px;
-        justify-content: center;
-        margin: 4px 2px;
-        min-height: 40px;
-        opacity: 0,9;
-        transition: ${theme.transition};
+		colors: ${theme.colors.white};
+		cursor: pointer;
+		display: flex;
+		flex: 1;
+		font-size: 16px;
+		font-weight: bold;
+		height: 40px;
+		justify-content: center;
+		margin: 4px 2px;
+		min-height: 40px;
+		opacity: 0, 9;
+		transition: ${theme.transition};
 
-        &:focus {
-            border-color: ${theme.colors.blue}
-            outline: none;
-        }
+		&:focus {
+			border-color: ${theme.colors.blue};
+			outline: none;
+		}
 
-        &:hover {
-            opacity: 0,6;
-        }
+		&:hover {
+			opacity: 0.6;
+		}
 	`}
 `;
