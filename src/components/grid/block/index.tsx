8b8fc551f2eafea27bc8ff11ef3f06f578119dfb
team-cake <eka.cheuk@gmail.com>
2020-11-12 11:46:06 +0100

    Number input by user is different in style from the puzzle numbers

--- src/components/grid/block/index.tsx
+++ src/components/grid/block/index.tsx
@@ -14,15 +14,18 @@ interface IProps {
 
 interface IState {
 	isActive: boolean;
+	isPuzzle: boolean;
 	value: N;
 }
 
 const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
 	const state = useSelector<IReducer, IState>(
-		({ workingGrid, selectedBlock }) => ({
+		({ challengeGrid, selectedBlock, workingGrid }) => ({
 			isActive: selectedBlock
 				? selectedBlock[0] === rowIndex && selectedBlock[1] === colIndex
 				: false,
+			isPuzzle:
+				challengeGrid && challengeGrid[rowIndex][colIndex] !== 0 ? true : false,
 			value: workingGrid ? workingGrid[rowIndex][colIndex] : 0,
 			// value: 0,
 		})
@@ -38,6 +41,7 @@ const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
 			active={state.isActive}
 			data-cy={`block-${rowIndex}-${colIndex}`}
 			onClick={handleClick}
+			puzzle={state.isPuzzle}
 		>
 			{state.value === 0 ? '' : state.value}
 		</Container>
